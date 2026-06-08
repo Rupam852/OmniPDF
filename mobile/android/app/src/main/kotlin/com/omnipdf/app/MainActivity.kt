cfbe7467c922f4f3ba7fcce956d404a198bc7934
rupambairagya44-max <rupambairagya08@gmail.com>
2026-06-08 12:21:55 +0530

    Fix mobile app package name to match google-services.json

---
+++ mobile/android/app/src/main/kotlin/com/omnipdf/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.omnipdf.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- mobile/android/app/src/main/kotlin/com/omnipdf/omnipdf/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.omnipdf.omnipdf
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
